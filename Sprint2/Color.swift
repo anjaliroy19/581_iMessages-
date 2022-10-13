6bf74b4cc17aad46ba0de4a84ed2ec142c4521ce
Anjali Roy <45764090+anjaliroy19@users.noreply.github.com>
2022-10-12 23:18:31 -0500

    Update Color.swift

--- Sprint2/Color.swift
+++ Sprint2/Color.swift
@@ -1,1 +1,16 @@
-
+If comments are written
+  Then color is white
+Else if symbol and operations are used
+  Then color is white
+Else If function/class name/variables
+  Then color is light green
+Else if numbers are used
+  Then color is red
+Else if directives/calls/keywords are used
+  Then color is green
+Else if character strings are used
+  Then color is light green
+Else if types are used
+  Then color is green
+Else
+  Color is white
