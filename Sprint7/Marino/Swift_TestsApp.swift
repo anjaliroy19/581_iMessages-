e897df5af40da1fbc13259580df495e53d9741e9
Morgan <89803467+marinome@users.noreply.github.com>
2023-01-29 23:20:37 -0600

    morgan upload

---
+++ Sprint7/Marino/Swift_TestsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Swift_TestsApp.swift
+//  Swift-Tests
+//
+//  Created by Morgan Marino on 12/2/22.
+//
+
+import SwiftUI
+
+@main
+struct Swift_TestsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
