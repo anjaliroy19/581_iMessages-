189d2d1e919e3650f25115dcbd2f53738fbefd30
dshakamuri <43610752+dshakamuri@users.noreply.github.com>
2023-02-11 18:51:11 -0600

    Add files via upload

---
+++ Sprint 8/Login Page/WelcomeScreenView.swift
@@ -0,0 +1,53 @@
+//
+//  WelcomeScreenView.swift
+//  login
+//
+//  Created by Divya Shakamuri on 02/10/23
+//
+
+import SwiftUI
+
+struct WelcomeScreenView: View {
+    var body: some View {
+        NavigationView {
+            ZStack {
+                Color("BgColor").edgesIgnoringSafeArea(.all)
+                VStack {
+                    Spacer()
+                    Image(uiImage: #imageLiteral(resourceName: "onboard"))
+                    Spacer()
+                    PrimaryButton(title: "Get Started")
+                    
+                    NavigationLink(
+                        destination: SignInScreenView().navigationBarHidden(true),
+                        label: {
+                            Text("Sign In")
+                                .font(.title3)
+                                .fontWeight(.bold)
+                                .foregroundColor(Color("PrimaryColor"))
+                                .padding()
+                                .frame(maxWidth: .infinity)
+                                .background(Color.white)
+                                .cornerRadius(50.0)
+                                .shadow(color: Color.black.opacity(0.08), radius: 60, x: 0.0, y: 16)
+                                .padding(.vertical)
+                        })
+                        .navigationBarHidden(true)
+                    
+                    HStack {
+                        Text("Need an account? ")
+                        Text("LOGIN")
+                            .foregroundColor(Color("PrimaryColor"))
+                    }
+                }
+                .padding()
+            }
+        }
+    }
+}
+
+struct WelcomeScreenView_Previews: PreviewProvider {
+    static var previews: some View {
+        WelcomeScreenView()
+    }
+}
