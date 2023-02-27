f12ea5944791dad8de06cb54c3b536fa3ae067ac
shpatel2001 <60803469+shpatel2001@users.noreply.github.com>
2023-02-26 22:30:07 -0600

    Add files via upload

---
+++ Sprint 9/ContentView.swift
@@ -0,0 +1,40 @@
+//
+//  ContentView.swift
+//  login
+//
+//  Created by Divya Shakamuri on 02/10/23
+//
+import SwiftUI
+
+struct ContentView: View {
+   
+    var body: some View {
+        WelcomeScreenView()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+
+struct PrimaryButton: View {
+    var title: String
+    var body: some View {
+        Text(title)
+            .font(.title3)
+            .fontWeight(.bold)
+            .foregroundColor(.white)
+            .frame(maxWidth: .infinity)
+            .padding()
+            .background(Color("PrimaryColor"))
+            .cornerRadius(50)
+    }
+}
+
+
+
+
+
+
