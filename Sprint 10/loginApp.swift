da105bba1bf9c785fda4da5381d2001a5a796228
dshakamuri <43610752+dshakamuri@users.noreply.github.com>
2023-03-12 17:11:58 -0500

    Create loginApp.swift

---
+++ Sprint 10/loginApp.swift
@@ -0,0 +1,18 @@
+
+
+//
+//  loginApp.swift
+//  login
+//
+//  Created by Divya Shakamuri on 02/10/23
+//
+import SwiftUI
+
+@main
+struct loginApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
